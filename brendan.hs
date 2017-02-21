d5d4886e131ea77ab3502ef515f08518551c0949
Brendan O'Brien <brendan.obrien@kroger.com>
2017-02-21 14:27:56 -0500

    Solve problem 4

--- brendan.hs
+++ brendan.hs
@@ -13,3 +13,13 @@ elementAt :: [a] -> Int -> a
 elementAt [] index = error "Empty list"
 elementAt xs 0 = error "No zeroth element"
 elementAt xs index = head $ drop (index-1) xs
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength xs =
+  let
+    inner :: [a] -> Int -> Int
+    inner [] len = len
+    inner xi len = inner (tail xi) (len + 1)
+  in
+   inner xs 0
