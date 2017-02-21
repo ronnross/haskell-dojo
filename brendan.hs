1e553cb42c8f67ef754ba565b463d3932b287b8d
Brendan O'Brien <brendan.obrien@kroger.com>
2017-02-21 17:25:53 -0500

    Refactor elementAt to use recursion but no inner function...it wasn't needed.

--- brendan.hs
+++ brendan.hs
@@ -1,27 +1,28 @@
 module BMO where
 
+-- Problem 1
 myLast :: [a] -> a
 myLast [] = error "Empty list"
 myLast [a] = a
 myLast (_:xs) = myLast xs
 
+-- Problem 2
 myButLast :: [a] -> a
 myButLast [] = error "Empty list"
 myButLast xs = (head . tail . reverse) xs
 
+-- Problem 3
 elementAt :: [a] -> Int -> a
 elementAt [] index = error "Empty list"
 elementAt xs 0 = error "No zeroth element"
--- elementAt xs index = head $ drop (index-1) xs
-elementAt xs index =
-  let
-    inner :: [a] -> Int -> a
-    inner list 0 = head list
-    inner [] index = error "Index out of range"
-    inner list index = inner (tail list) (index - 1)
-  in
-    inner xs (index - 1)
+elementAt xs index = head $ drop (index-1) xs
 
+elementAt2 :: [a] -> Int -> a
+elementAt2 [] index = error "Index out of range"
+elementAt2 list 1 = head list
+elementAt2 list index = elementAt2 (tail list) (index - 1)
+
+-- Problem 4
 myLength :: [a] -> Int
 myLength xs =
   let
@@ -31,6 +32,7 @@ myLength xs =
   in
    inner xs 0
 
+-- Problem 5
 myReverse :: [a] -> [a]
 myReverse xs =
   let
